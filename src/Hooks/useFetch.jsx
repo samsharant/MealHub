ae76dbf452fc32ed56c27102de4eb0196bc7d3e9
sam <sam.sharan@valuebound.com>
2024-07-20 13:02:15 +0530

    Added a custom hook (useFetch)

---
+++ src/Hooks/useFetch.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useFetch(url) {
+  const [responseData, setResponseData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        if (response.data) setResponseData(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { responseData, loading, error };
+}
+
+export default useFetch;
